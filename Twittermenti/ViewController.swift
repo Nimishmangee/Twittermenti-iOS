30ba4ad3ef4341749012cfa32b2d1ccbdcb93074
Nimishmangee <82581324+Nimishmangee@users.noreply.github.com>
2022-07-18 01:22:53 +0530

    Initial commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Angela Yu on 17/07/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import SwifteriOS
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter=Swifter(consumerKey: "KNOfcg86mhfxzukCE8dyH60W5", consumerSecret: "uwR4WAQDqixctJvrcvfmmGQKp1LJfHZ1hlpJBqupW1vw9Tbctl");
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        swifter.searchTweet(using: "@Apple", lang:"en", count: 100) { results, searchMetadata in
+            print(results);
+        } failure: { error in
+            print("There was an error with the Twitter API Request \(error)");
+        }
+
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+        
+        
+    }
+    
+}
+
