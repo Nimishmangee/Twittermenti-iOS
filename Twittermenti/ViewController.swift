ec1ffb2ecae93b9e6e45486887cddc662e701d88
Nimishmangee <82581324+Nimishmangee@users.noreply.github.com>
2022-07-18 02:29:47 +0530

    Podfile Added

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
@@ -15,12 +16,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    let sentimentClassifier: TweetSentimentClassifier = {
+        do {
+            let config = MLModelConfiguration()
+            return try TweetSentimentClassifier(configuration: config)
+        } catch {
+            print(error)
+            fatalError("Couldn't create TweetSentimentClassifier")
+        }
+    }()
+    
     let swifter=Swifter(consumerKey: "KNOfcg86mhfxzukCE8dyH60W5", consumerSecret: "uwR4WAQDqixctJvrcvfmmGQKp1LJfHZ1hlpJBqupW1vw9Tbctl");
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        swifter.searchTweet(using: "@Apple", lang:"en", count: 100) { results, searchMetadata in
-            print(results);
+        
+        let prediction = try! sentimentClassifier.prediction(text: "Apple is bad")
+         
+        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang:"en", count: 100, tweetMode: .extended) { results, searchMetadata in
+            //print(results);
         } failure: { error in
             print("There was an error with the Twitter API Request \(error)");
         }
@@ -28,8 +44,6 @@ class ViewController: UIViewController {
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-        //Currently it does nothing
-        //Decided by Angela
         
     }
     
